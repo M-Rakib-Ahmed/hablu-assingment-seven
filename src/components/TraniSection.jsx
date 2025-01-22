d369cfdadfe69c70b3b5b9dc1de42c95d2bd52bf
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-01-22 13:55:23 +0600

    new section added

--- src/components/TraniSection.jsx
+++ src/components/TraniSection.jsx
@@ -1,7 +1,7 @@
 import voctor from '../assets/Vector.png'
 const TraniSection = () => {
     return (
-        <div className="text-center shadow-2xl bg-[#07292F] text-white p-10">
+        <div className="text-center shadow-2xl bg-[#07292F] text-white p-10 hover:scale-105 duration-150">
             <h2 className="text-3xl font-bold">Train your aiDR on your...</h2>  
             <p className="font-serif text-2xl text-[#0DBFC4]">prefered email st|</p>
             <p className="mt-5">You’re in control. Train your aiDR on <br /> elements of your Marketing strategy.</p>
